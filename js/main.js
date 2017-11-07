ffd66845a6c42da31db14b48715637b17851e627
gabycasti <castillg@gmail.com>
2017-11-07 12:38:01 -0300

    trello

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+//Creando las variables
+
+var lista_formulario = getElementById('lista');
+var boton = document.createElement('boton');
+var texto = document.createTextNodo("Guardar")
+lista_formulario.appenChild(boton);
+boton.appenChild(texto);
+
+
+lista_formulario.addEventListener('click', function()){
+
+	lista_formulario.addEventListener('click',function(){
+         
+
+	})
+
+}
